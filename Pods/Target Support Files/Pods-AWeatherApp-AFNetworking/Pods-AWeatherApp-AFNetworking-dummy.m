14c4e2382507a45378125a3e965e67774f0ed727
Andrea Ciani <cianiandrea10@gmail.com>
2015-05-19 20:55:25 +0200

    Added cocoapod files

---
+++ Pods/Target Support Files/Pods-AWeatherApp-AFNetworking/Pods-AWeatherApp-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AWeatherApp_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_AWeatherApp_AFNetworking
+@end
---
+++ Pods/Target Support Files/Pods-AWeatherApp/Pods-AWeatherApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AWeatherApp : NSObject
+@end
+@implementation PodsDummy_Pods_AWeatherApp
+@end
